dbb794e86ffe94d79a0581ca5910825da1c8d405
rama707708 <rama707708@gmail.com>
2025-03-07 00:48:17 +0530

    Update userInfo.ts

--- userInfo.ts
+++ userInfo.ts
@@ -76,3 +76,15 @@ export class UserComponent implements OnInit {
   }
 }
 
+
+
+// for token
+
+try {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    return JSON.parse(atob(base64));
+  } catch (error) {
+    console.error('Invalid JWT token:', error);
+    return null;
+  }
